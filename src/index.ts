c0799cde7e6eca5acfc44264ae08b3b5d6fef92a
AymaneBelghacham <160013032+AymaneBelghacham@users.noreply.github.com>
2024-02-26 14:05:49 +0100

    feat: [DevOps-1] Initialize RabbitMQ Client Package (#1)
    Co-authored-by: Aymanebelg <aymane.belghacham@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
 import express from 'express'
-import userRoutes from './routes/userRoutes'
-import connectDB from './database/db'
 import swaggerUi from 'swagger-ui-express'
 import * as swaggerDocument from './swagger.json'
 
 const app = express()
 
-connectDB().then(() => {
-  console.log('Connected to MongoDB, starting server...')
+app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-  app.use(express.json())
-  app.use('/', userRoutes)
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-  const PORT = process.env.PORT ?? 3000
-  app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
-}).catch((error) => {
-  console.error('Failed to connect to MongoDB:', error)
-})
+const PORT = process.env.PORT ?? 3000
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
