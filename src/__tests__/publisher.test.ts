eb8a1e9b4e6465dc095da5df502917bb0f8fa66d
Aymanebelg <aymane.belghacham@gmail.com>
2024-02-28 13:49:15 +0100

    DevOps-2 : Implement Publisher Functionality5

--- src/__tests__/publisher.test.ts
+++ src/__tests__/publisher.test.ts
@@ -1,9 +1,5 @@
 import { sendMessage } from '../publisher/index'
 import * as amqp from 'amqplib'
-import * as dotenv from 'dotenv'
-
-// Charger les variables d'environnement depuis le fichier .env
-dotenv.config()
 
 jest.mock('amqplib')
 
@@ -17,7 +13,7 @@ describe('sendMessage', () => {
     const routingKey = 'test_routing_key'
     const message = 'Test message'
     const apiKey = 'test_api_key'
-    const rabbitMQUrl = process.env.RABBIT_MQ_URL ?? '' // Utilisation de l'opérateur de coalescence nulle pour fournir une valeur par défaut
+    const rabbitMQUrl = 'amqp://localhost'
 
     const mockedChannel: any = {
       assertExchange: jest.fn(),
--- src/publisher/index.ts
+++ src/publisher/index.ts
@@ -1,25 +1,25 @@
 import * as amqp from 'amqplib'
 import dotenv from 'dotenv'
-import path from 'path'
+import * as path from 'path'
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') })
-export async function sendMessage (exchange: string, routingKey: string, message: string, apiKey: string): Promise<void> {
+
+export async function sendMessage (
+  exchange: string,
+  routingKey: string,
+  message: string,
+  apiKey: string
+): Promise<void> {
   let connection: amqp.Connection | null = null
   let channel: amqp.Channel | null = null
 
   try {
-    const rabbitMQUrl = process.env.RABBIT_MQ_URL ?? ''
-    if (rabbitMQUrl === '') {
-      throw new Error('RabbitMQ URL is not provided in the environment variables.')
-    }
-
+    const rabbitMQUrl = process.env.RABBIT_MQ_URL ?? '' // Default URL
     connection = await amqp.connect(rabbitMQUrl)
     channel = await connection.createChannel()
 
     await channel.assertExchange(exchange, 'direct', { durable: true })
     channel.publish(exchange, routingKey, Buffer.from(message))
-
-    console.log(`Sent message '${message}' with API key '${apiKey}' to exchange '${exchange}' with routing key '${routingKey}'`)
   } finally {
     if (channel != null) {
       await channel.close()
