9e38964452f800a67f3b142d23401d1262e21eb1
Aymanebelg <aymane.belghacham@gmail.com>
2024-03-13 10:27:24 +0000

    removed unused fields

--- src/config/config.test.ts
+++ src/config/config.test.ts
@@ -3,8 +3,6 @@ import * as path from 'path'
 dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 interface Config {
-  port: string | number
-  mongodbUri: string
   rabbitmqurl_test: string
   client_cert_test: string
   client_key_test: string
@@ -14,8 +12,6 @@ interface Config {
 }
 
 const config: Config = {
-  port: 3000,
-  mongodbUri: 'mongodb://localhost:27017/defaultDb',
   rabbitmqurl_test: 'amqps://localhost:5671',
   client_cert_test: '<rootDir>/src/ssl_cert/ca_certificate.pem',
   client_key_test: '<rootDir>/src/ssl_cert/client_private_key.pem',
